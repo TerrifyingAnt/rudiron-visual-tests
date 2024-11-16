771d92271c3d6bc8f27587112715424d24c441b8
Ant <TerrifyingAnt@yandex.ru>
2024-11-17 01:54:16 +0300

    added styles

--- src/extension.ts
+++ src/extension.ts
@@ -13,22 +13,35 @@ export function activate(context: vscode.ExtensionContext) {
                     enableScripts: true,
                     localResourceRoots: [
                         vscode.Uri.file(path.join(context.extensionPath, 'out')),
-                        vscode.Uri.file(path.join(context.extensionPath, 'src', 'webview'))
+                        vscode.Uri.file(path.join(context.extensionPath, 'src', 'webview')),
+                        vscode.Uri.file(path.join(context.extensionPath, 'src', 'components', 'styles'))
                     ]
                 }
             );
 
             const reactAppPath = path.join(context.extensionPath, 'out', 'react-app.js');
-            const stylesPath = path.join(context.extensionPath, 'src', 'webview', 'styles.css');
+
             const htmlPath = path.join(context.extensionPath, 'src', 'webview', 'webview.html');
+
+            const styleBase = path.join(context.extensionPath, 'src', 'components', 'styles', 'base.css')
+            const styleBlocks = path.join(context.extensionPath, 'src', 'components', 'styles', 'blocks.css')
+            const styleButtons = path.join(context.extensionPath, 'src', 'components', 'styles', 'button.css')
+            const styleWorkspace = path.join(context.extensionPath, 'src', 'components', 'styles', 'workspace.css')
 
             let htmlContent = fs.readFileSync(htmlPath, 'utf8');
             const reactAppUri = panel.webview.asWebviewUri(vscode.Uri.file(reactAppPath));
-            const stylesUri = panel.webview.asWebviewUri(vscode.Uri.file(stylesPath));
+
+            const stylesUriBase = panel.webview.asWebviewUri(vscode.Uri.file(styleBase));
+            const stylesUriBlocks = panel.webview.asWebviewUri(vscode.Uri.file(styleBlocks));
+            const stylesUriButtons = panel.webview.asWebviewUri(vscode.Uri.file(styleButtons));
+            const stylesUriWorkspace = panel.webview.asWebviewUri(vscode.Uri.file(styleWorkspace));
 
             // Заменяем плейсхолдеры в HTML
             htmlContent = htmlContent.replace('%REACT_APP%', reactAppUri.toString());
-            htmlContent = htmlContent.replace('%STYLESHEET%', stylesUri.toString());
+            htmlContent = htmlContent.replace('%STYLESHEET_BASE%', stylesUriBase.toString());
+            htmlContent = htmlContent.replace('%STYLESHEET_BLOCKS%', stylesUriBlocks.toString());           
+            htmlContent = htmlContent.replace('%STYLESHEET_BUTTONS%', stylesUriButtons.toString());          
+            htmlContent = htmlContent.replace('%STYLESHEET_WORKSPACE%', stylesUriWorkspace.toString());
 
             panel.webview.html = htmlContent;
         })
